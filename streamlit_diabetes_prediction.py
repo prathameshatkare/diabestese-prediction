f89c14436e6e217751cd3c59c5b04f95f331fea6
IronCoder <142227723+prathameshatkare@users.noreply.github.com>
2024-04-01 15:38:25 +0530

    Update streamlit_diabetes_prediction.py

--- streamlit_diabetes_prediction.py
+++ streamlit_diabetes_prediction.py
@@ -1,7 +1,6 @@
-# importing Important Liberaries
+# importing Important Libraries
 import pickle
 import streamlit as st
-import numpy as np
 
 # Load model
 model_diabetes = pickle.load(open('model_diabetes.sav', 'rb'))
@@ -12,41 +11,31 @@ st.title('Diabetes Prediction')
 # Split Columns
 col1, col2 = st.columns(2)
 
-with col1 :
-  Pregnancies = st.number_input('Enter the Pregnancies value')
+# Input fields with validation
+# Input fields with validation
+with col1:
+    pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1)
+    glucose = st.number_input('Glucose', min_value=0, max_value=300, step=1)
+    blood_pressure = st.number_input('Blood Pressure', min_value=0, max_value=200, step=1)
+    skin_thickness = st.number_input('Skin Thickness', min_value=0, max_value=100, step=1)
+with col2:
+    insulin = st.number_input('Insulin', min_value=0, max_value=1000, step=1)
+    bmi = st.number_input('BMI', min_value=0.0, max_value=60.0, step=0.1)
+    diabetes_pedigree_function = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.5, step=0.01)
+    age = st.number_input('Age', min_value=0, max_value=120, step=1)
 
-with col2 :
-  Glucose = st.number_input('Enter the Glucose value')
-  
-with col1 :
-  BloodPressure = st.number_input('Enter the Blood Pressure value')
-
-with col2 :
-  SkinThickness = st.number_input('Enter the Skin Thickness value')
-
-with col1 :
-  Insulin = st.number_input('Enter the Insulin value')
-
-with col2 :
-  BMI = st.number_input('Enter the BMI value')
-
-with col1 :
-  DiabetesPedigreeFunction = st.number_input('Enter the Diabetes Pedigree Function value')
-
-with col2 :
-  Age = st.number_input('Enter the Age value')
-  
 # Prediction
 diabetes_diagnosis = ''
 
 if st.button('Diabetes Prediction Test'):
-  diabetes_prediction = model_diabetes.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-  
-  if(diabetes_prediction[0]==1):
-    diabetes_diagnosis = 'The patient has diabetes'
-  else :
-    diabetes_diagnosis = 'The patient does not have diabetes'
- 
+    if pregnancies == 0 or glucose == 0 or blood_pressure == 0 or skin_thickness == 0 or insulin == 0 or bmi == 0.0 or diabetes_pedigree_function == 0.0 or age == 0:
+        st.warning("Please fill in all the fields.")
+    else:
+        diabetes_prediction = model_diabetes.predict([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
+        
+        if diabetes_prediction[0] == 1:
+            diabetes_diagnosis = 'The patient has diabetes'
+        else:
+            diabetes_diagnosis = 'The patient does not have diabetes'
 
-st.success(diabetes_diagnosis)
-
+        st.success(diabetes_diagnosis)
